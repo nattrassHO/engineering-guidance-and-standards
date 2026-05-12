c72f9f64fd80d7d5cf9b375e9d895f7871d0fa48
Paul Nattrass <paul.nattrass1@homeoffice.gov.uk>
2026-05-12 15:29:17 +0100

    refactor: enhance TEST_ROOT_URL method for improved URL handling

--- tests/support/testing_params.js
+++ tests/support/testing_params.js
@@ -4,6 +4,22 @@ export const testing_params = {
   TEST_PATH: process.env.TEST_PATH ?? '',
   
   get TEST_ROOT_URL() {
-    return `${this.TEST_URL}:${this.TEST_PORT}${this.TEST_PATH.replace(/\/$/, '')}`;
+    let normalizedUrl = this.TEST_URL.trim();
+    if (!/^https?:\/\//i.test(normalizedUrl)) {
+      normalizedUrl = `http://${normalizedUrl}`;
+    }
+
+    const parsedUrl = new URL(normalizedUrl);
+    if (!parsedUrl.port && this.TEST_PORT) {
+      parsedUrl.port = this.TEST_PORT;
+    }
+
+    const basePath = parsedUrl.pathname === '/' ? '' : parsedUrl.pathname.replace(/\/$/, '');
+    const extraPath = this.TEST_PATH
+      ? `/${this.TEST_PATH.replace(/^\/+|\/+$/g, '')}`
+      : '';
+
+    parsedUrl.pathname = `${basePath}${extraPath}` || '/';
+    return parsedUrl.toString().replace(/\/$/, '');
   }
 };
